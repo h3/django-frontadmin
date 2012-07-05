8cd8e2643316f31f1438907085dc422265870dd3
Maxime Haineault <max@motion-m.ca>
2012-07-05 11:09:05 -0400

    Updated frontadmin plugin base

--- frontadmin/plugins.py
+++ frontadmin/plugins.py
@@ -1,11 +1,18 @@
 
 
 class PluginBase(object):
-    button_template = '<a id="%(id)s" href="%(url)s" title="%(title)s" class="button %(classname)s" target="_parent">%(label)s</a>'
+    button_template = '<a id="%(id)s" href="%(url)s" title="%(title)s" class="button %(classname)s" target="%(target)s">%(label)s</a>'
 
     def __init__(self, request):
         self.request = request
 
     def button(self, **kwargs):
-        return self.button_template % kwargs
+        ctx = {
+            'target': '_parent',
+            'title': '',
+            'id': '',
+            'classname': '',
+        }
+        ctx.update(kwargs)
+        return self.button_template % ctx
         
