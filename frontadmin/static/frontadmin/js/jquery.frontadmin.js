baa51672bb82d3340ce90eea26a7b4204b2684d3
Dominic Montreuil <dominic.montreuil@gmail.com>
2012-07-17 15:44:50 -0400

    fixed issue when a form error would occur and iframe would remain hidden. fixed cancel button creation/visibility when editing.

--- frontadmin/static/frontadmin/js/jquery.frontadmin.js
+++ frontadmin/static/frontadmin/js/jquery.frontadmin.js
@@ -129,11 +129,19 @@ $(function(){
         $self.cleanDocument = function(d) {
             // add cancel button
             var $this  = this;
-            var cancel = $('<li class="cancel-button-container"><a class="cancel-link" href="#">Cancel</a></li>')
-            cancel.find('a').bind('click.adminToolbar', function() {
+            var cancel = $('<li class="cancel-button-container"><input class="grp_button" type="submit" name="_cancel" value="Cancel" style="background-color:#444;font-color:#2B8AAB;background:-moz-linear-gradient(center top, #666, #444) repeat scroll 0 0 transparent;border:none;"></li>')
+            cancel.find('input').bind('click.adminToolbar', function() {
                 $self.closeActiveFrame();
                 return false;
             })
+            cancel.find('input').hover(
+                function(){
+                    $(this).css('background', '-moz-linear-gradient(center top, #E3E3E3, #D6D6D6) repeat scroll 0 0 transparent')
+                },
+                function(){
+                    $(this).css('background', '-moz-linear-gradient(center top, #666, #444) repeat scroll 0 0 transparent')
+                }
+            );
             if (d.find('#changelist').get(0)) {
                var changelist = true;
                if (!d.find('#submit').find('.submit-row').get(0)) {
@@ -150,13 +158,14 @@ $(function(){
 
             d.find('#header, #breadcrumbs').remove().end()
              .find('body').css({paddingTop: 0}).end()
-             .find('.module.footer')
+             //.find('.module.footer')
+             .find('.grp-submit-row')
                  .css({
                       '-moz-border-radius': '0 0 4px 4px',
                       '-webkit-border-radius': '0 0 4px 4px',
                       'border-radius': '0 0 4px 4px'
                   })
-                 .find('.submit-row').append(cancel).end()
+                 .find('ul').append(cancel).end()
                  .find('input[name="_continue"]').bind('click', function() {
                      $self.states.active_frame.addClass('saving continue')
                      $self.states.active_frame.parent().addClass('saving continue')
@@ -252,6 +261,9 @@ $(function(){
                                 frame.parent().removeClass('saving');
                                 frame.removeClass('saving').removeClass('continue');
                             }
+                        }
+                        else {
+                            frame.removeClass('saving');
                         }
                     }
                     else if ($(this).hasClass('deleting')) {
